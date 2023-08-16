3c62f5158aba88f3215716a6070ed3c0b02e9105
Oleksandr Ismailov <oleksandr.ismailov@protonbmail.com>
2023-08-16 14:14:33 +0200

    Added builf script for building package.
    Signed-off-by: Oleksandr Ismailov <oleksandr.ismailov@protonbmail.com>

--- build_macos.sh
+++ build_macos.sh
@@ -1,2 +1,16 @@
-cmake -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_TEST=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_INSTALL_PREFIX=./libhuestream_install/
+build_type="Debug"
+if [ -z "$1" ]; then
+  echo "Build type " $build_type
+else
+  build_type=$1
+fi
+
+additional_cmake_options="-DCMAKE_INSTALL_PREFIX=./libhuestream_install/"
+if [ -z "$2" ]; then
+  echo $additional_cmake_options
+else
+  additional_cmake_options=$2
+fi
+
+cmake -Bbuild -DBUILD_TEST=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_BUILD_TYPE=$build_type $additional_cmake_options
 cmake --build build --target install
